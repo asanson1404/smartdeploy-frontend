8270b28918ee9e93fb62dfe007cbc0a7afbd68a2
rickknowlton <richard.d.knowlton@gmail.com>
2023-06-09 15:23:43 -0400

    fix: added just netlify plugin

--- netlify-plugin-just-install.cjs
+++ netlify-plugin-just-install.cjs
@@ -1,20 +1,20 @@
-const { execSync } = require("child_process");
+const { spawnSync } = require('child_process');
 
 module.exports = {
   async onPreBuild({ utils }) {
     try {
-      // Run the 'just generate' command using execSync
-      execSync("npx just generate", { stdio: "inherit" });
+      // Run the 'npx just generate' command using spawnSync
+      spawnSync('npx', ['just', 'generate'], { stdio: 'inherit' });
     } catch (error) {
-      utils.build.failBuild("Failed to run `just generate` command", { error });
+      utils.build.failBuild('Failed to run `just generate` command', { error });
     }
   },
   async onBuild({ utils }) {
     try {
       // Run the 'npm run build' command
-      await utils.run.command("npm run build");
+      await utils.run.command('npm run build');
     } catch (error) {
-      utils.build.failBuild("Failed to run `npm run build` command", { error });
+      utils.build.failBuild('Failed to run `npm run build` command', { error });
     }
   },
 };
