35d9bbe7c8ad06b4d29e0dbfcf59a8f61244ea40
rickknowlton <richard.d.knowlton@gmail.com>
2023-06-09 13:21:23 -0400

    cleanup: extraneous code from smartdeploy landing

--- src/components/Copyright.js
+++
@@ -1,9 +0,0 @@
-const Copyright = () => {
-    const currentYear = new Date().getFullYear();
-
-    return (
-        <p className="uk-text-muted">© {currentYear} SmartDeploy. All rights reserved.</p>
-    );
-};
-
-export default Copyright;
