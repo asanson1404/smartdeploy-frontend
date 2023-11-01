26162b43dadc506775626e4a769c4cfd06b74d50
asanson <alexandresanson.pro@gmail.com>
2023-11-01 17:56:13 +0100

    fix: prepare next for deployment on GH pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
+  output: "export",
+  basePath: process.env.NODE_ENV === "production" ? "": undefined,
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: false,
 }
-
-module.exports = nextConfig
