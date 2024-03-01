8b0755c94d7b7d1937a2a53c1ca32f0de836100f
asanson <alexandresanson.pro@gmail.com>
2024-03-02 01:39:18 +0800

    feat: Update PopUp to subscribe to automatic bump

--- components/published-tab/deploy-components.tsx
+++ components/published-tab/deploy-components.tsx
@@ -3,7 +3,7 @@ import styles from './style.module.css'
 import { useThemeContext } from '../../context/ThemeContext'
 import { useWalletContext } from '../../context/WalletContext'
 import { FetchDatas } from "@/pages"
-import { useState, ChangeEvent, Dispatch, SetStateAction } from 'react'
+import { useState, ChangeEvent, Dispatch, SetStateAction, useEffect } from 'react'
 import Popup from 'reactjs-popup'
 import Dropdown from 'react-dropdown'
 import { BsSendPlus } from 'react-icons/bs'
@@ -28,9 +28,29 @@ function DeployIconComponent(props: DeployVersionProps) {
     const [wouldDeploy, setWouldDeploy]   = useState<boolean>(false); 
     const [deployedName, setDeployedName] = useState<string>("");
     const [isDeploying, setIsDeploying]   = useState<boolean>(false);
+    const [bumping, setBumping] = useState<boolean | null>(null);
+    const [showBumpingPopup, setShowBumpingPopup] = useState<boolean>(false);
+    const [conditionalDeployButtonCss, setConditionalDeployButtonCss] = useState<boolean>(false);
+
+    useEffect(() => {
+        if (bumping != null) {
+            setConditionalDeployButtonCss(true);
+        } else {
+            setConditionalDeployButtonCss(false);
+        }
+    }, [bumping]);
 
     const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
         setDeployedName(e.target.value);
+    }
+
+    const deployData: DeployArgsObj = {
+        contract_name: props.contract_name,
+        version: props.selected_version.version,
+        deployed_name: deployedName,
+        owner: walletContext.address,
+        salt: undefined,
+        init: undefined
     }
   
     return (
@@ -49,12 +69,12 @@ function DeployIconComponent(props: DeployVersionProps) {
                     </td>
                     <Popup  open={wouldDeploy} closeOnDocumentClick={false}>
                         <div className={styles.popupContainer} data-theme={activeTheme}>
-                            <button className={styles.close} onClick={() => setWouldDeploy(false)}>
+                            <button className={styles.close} onClick={() => {setWouldDeploy(false), setBumping(null)}}>
                                 &times;
                             </button>
                             <div className={styles.header}>Deploy <span className={styles.nameColor} data-theme={activeTheme}>{props.contract_name} ({props.selected_version.version_string})</span> </div>
                             <div className={styles.content}>
-                                <p className={styles.mainMessage}><b>You are about to create an instance of <span className={styles.nameColor} data-theme={activeTheme}>{props.contract_name}</span> published contract where you will be the owner.</b><br/></p>
+                                <p className={styles.mainMessage}><b>You are about to create an instance of <span className={styles.nameColor} data-theme={activeTheme}>{props.contract_name} ({props.selected_version.version_string})</span> where you will be the owner.</b><br/></p>
                                 <div className={styles.deployedNameDiv}>
                                     <b>Please choose a contract instance name:</b>
                                     <input 
@@ -67,24 +87,38 @@ function DeployIconComponent(props: DeployVersionProps) {
                                         onChange={handleInputChange}>
                                     </input>
                                 </div>
+                                <div className={styles.bumpContainer}>
+                                    <p>Do you want SmartDeploy to automatically bump your contract instance when it arrives at expiration time?</p>
+                                    <div className={styles.checkboxContainer}>
+                                        <div>
+                                            <input 
+                                                type='checkbox'
+                                                onChange={() => { if(bumping !== true) setBumping(true)} }
+                                                onClick={() => { if(bumping === true) setBumping(null)} }
+                                                checked={bumping === true}
+                                            />
+                                            <label>Yes, I agree to pay bumping fees now</label>
+                                        </div>
+                                        <div>
+                                            <input 
+                                                type='checkbox'
+                                                onChange={() => { if(bumping !== false) setBumping(false)} }
+                                                onClick={() => { if(bumping === false) setBumping(null)} }
+                                                checked={bumping === false}
+                                            />
+                                            <label>No</label>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                             <div className={styles.buttonContainer}>
-                                {!isDeploying ? (
+                                {(!isDeploying && !bumping) ? (
                                     <>
-                                        <button className={styles.button} 
+                                        <button className={conditionalDeployButtonCss ? styles.button : styles.buttonDisabled}
                                                 data-theme={activeTheme}
                                                 onClick={() => {
 
                                                     setIsDeploying(true);
-
-                                                    const argsObj: DeployArgsObj = {
-                                                        contract_name: props.contract_name,
-                                                        version: props.selected_version.version,
-                                                        deployed_name: deployedName,
-                                                        owner: walletContext.address,
-                                                        salt: undefined,
-                                                        init: undefined
-                                                    }
 
                                                     deploy(
                                                         walletContext,
@@ -92,15 +126,39 @@ function DeployIconComponent(props: DeployVersionProps) {
                                                         setIsDeploying,
                                                         setDeployedName,
                                                         setWouldDeploy,
-                                                        argsObj
+                                                        deployData
                                                     );
                                                 }}
+                                                disabled={bumping === null}
                                         >
                                             Deploy
                                         </button>
-                                        <button className={styles.button} data-theme={activeTheme} onClick={() => setWouldDeploy(false)}>
+                                        <button className={styles.button} data-theme={activeTheme} onClick={() => {setWouldDeploy(false); setBumping(null)}}>
                                             Cancel
                                         </button>
+                                    </>
+                                ) : (!isDeploying && bumping) ? (
+                                    <>
+                                        <button className={conditionalDeployButtonCss ? styles.button : styles.buttonDisabled}
+                                                data-theme={activeTheme}
+                                                onClick={() => {
+                                                    if(deployData.deployed_name == "") {
+                                                        window.alert("Contract name not set. Please enter a name for your contract instance before continuing.")
+                                                    } else {
+                                                        setShowBumpingPopup(true)
+                                                    }
+                                                }}
+                                                disabled={bumping === null}
+                                        >
+                                            Continue
+                                        </button>
+                                        <button className={styles.button} data-theme={activeTheme} onClick={() => {setWouldDeploy(false); setBumping(null)}} disabled={showBumpingPopup}>
+                                            Cancel
+                                        </button>
+
+                                        {showBumpingPopup && (
+                                            <BumpingPopup showBumpingPopup={showBumpingPopup} setShowBumpingPopup={setShowBumpingPopup} deployData={deployData}/>
+                                        )}
                                     </>
                                 ) : (
                                     <button className={styles.buttonWhenDeploying} data-theme={activeTheme}>
@@ -114,6 +172,119 @@ function DeployIconComponent(props: DeployVersionProps) {
             )}
         </>
     );
+}
+
+function BumpingPopup({
+    showBumpingPopup,
+    setShowBumpingPopup,
+    deployData
+}: {
+    showBumpingPopup: boolean
+    setShowBumpingPopup: Dispatch<SetStateAction<boolean>>,
+    deployData: DeployArgsObj
+}) {
+
+    const { activeTheme } = useThemeContext();
+    const walletContext = useWalletContext();
+
+    enum BumpingSubscription {
+        ONE_MONTH,
+        SIX_MONTHS,
+        ONE_YEAR,
+    }
+
+    const [isDeploying, setIsDeploying] = useState(false);
+    const [bumpingSubscription, setBumpingSubscription] = useState<BumpingSubscription | null>(null);
+    const [conditionalDeployButtonCss, setConditionalDeployButtonCss] = useState<boolean>(false);
+
+    useEffect(() => {
+        if (bumpingSubscription != null) {
+            setConditionalDeployButtonCss(true);
+        } else {
+            setConditionalDeployButtonCss(false);
+        }
+    }, [bumpingSubscription]);
+
+    return (
+        <div style={{position: 'absolute', top: 20, left: 20}}>
+
+        
+        <Popup  open={showBumpingPopup} closeOnDocumentClick={false} offsetX={500} offsetY={-150} trigger={<div></div>} arrow={false}>
+            <div className={styles.bumpingPopupContainer} data-theme={activeTheme}>
+                <button className={styles.close} onClick={() => {setBumpingSubscription(null); setShowBumpingPopup(false)}}>
+                    &times;
+                </button>
+                <div className={styles.header}>Bumping subscription for contract instance <span className={styles.nameColor} data-theme={activeTheme}>{deployData.deployed_name}</span> </div>
+                <div className={styles.content}>
+                    <div className={styles.bumpContainer}>
+                        <p>How long do you want us to keep your contract instance alive?</p>
+                        <div className={styles.checkboxContainer}>
+                            <div>
+                                <input 
+                                    type='checkbox'
+                                    onChange={() => { if(bumpingSubscription !== BumpingSubscription.ONE_MONTH) setBumpingSubscription(BumpingSubscription.ONE_MONTH)} }
+                                    onClick={() => { if(bumpingSubscription === BumpingSubscription.ONE_MONTH) setBumpingSubscription(null)} }
+                                    checked={bumpingSubscription === BumpingSubscription.ONE_MONTH}
+                                />
+                                <label>1 month (xxx XLM)</label>
+                            </div>
+                            <div>
+                                <input 
+                                    type='checkbox'
+                                    onChange={() => { if(bumpingSubscription !== BumpingSubscription.SIX_MONTHS) setBumpingSubscription(BumpingSubscription.SIX_MONTHS)} }
+                                    onClick={() => { if(bumpingSubscription === BumpingSubscription.SIX_MONTHS) setBumpingSubscription(null)} }
+                                    checked={bumpingSubscription === BumpingSubscription.SIX_MONTHS}
+                                />
+                                <label>6 month (xxx XLM)</label>
+                            </div>
+                            <div>
+                                <input 
+                                    type='checkbox'
+                                    onChange={() => { if(bumpingSubscription !== BumpingSubscription.ONE_YEAR) setBumpingSubscription(BumpingSubscription.ONE_YEAR)} }
+                                    onClick={() => { if(bumpingSubscription === BumpingSubscription.ONE_YEAR) setBumpingSubscription(null)} }
+                                    checked={bumpingSubscription === BumpingSubscription.ONE_YEAR}
+                                />
+                                <label>1 year (xxx XLM)</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.buttonContainer}>
+                    {!isDeploying ? (
+                        <>
+                            <button className={conditionalDeployButtonCss ? styles.button : styles.buttonDisabled}
+                                    data-theme={activeTheme}
+                                    onClick={() => {
+
+                                        setIsDeploying(true);
+
+                                        //deploy(
+                                        //    walletContext,
+                                        //    deployData.refetchDeployedContract,
+                                        //    setIsDeploying,
+                                        //    setDeployedName,
+                                        //    setWouldDeploy,
+                                        //    deployData
+                                        //);
+                                    }}
+                                    disabled={bumpingSubscription === null}
+                            >
+                                Deploy
+                            </button>
+                            <button className={styles.button} data-theme={activeTheme} onClick={() => {setBumpingSubscription(null); setShowBumpingPopup(false)}}>
+                                Cancel
+                            </button>
+                        </>
+                    ) : (
+                        <button className={styles.buttonWhenDeploying} data-theme={activeTheme}>
+                            Deploying...
+                        </button>
+                    )}
+                </div>
+            </div>
+        </Popup>
+        </div>
+    )
 }
 
 type DeployProps = {
